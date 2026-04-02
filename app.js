2cab34305296a421ae80fa678823cab7fa482c8d
indraphand-mb <indra.phand@mindbowser.com>
2026-04-02 18:23:55 +0530

    fix : version issue

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
+const db = require('./util/database')
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,6 +14,12 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('select * from products').then((data)=>{
+console.log(data[0],data[1]);
+
+}).catch((err)=>{console.log(err);
+})
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
