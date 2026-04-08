a6919e142b2f88d5b4c9fc756a9c8af678a3ed72
indraphand-mb <indra.phand@mindbowser.com>
2026-04-08 19:55:12 +0530

    feature : Add cart logic done

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,5 @@
 const Product = require('../models/product');
 
-const mongodb = require("mongodb")
-
-const ObjectId = mongodb.ObjectId
-
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
@@ -17,11 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-
-  const product = new Product(title,price,description,imageUrl)
-
-  
-   product.save().then(result => {
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
+  product
+    .save()
+    .then(result => {
       // console.log(result);
       console.log('Created Product');
       res.redirect('/admin/products');
@@ -37,11 +39,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-    Product.findById(prodId)
-    // .getProducts({ where: { id: prodId } })
-    // // Product.findById(prodId)
+  Product.findById(prodId)
+    // Product.findById(prodId)
     .then(product => {
-    
       if (!product) {
         return res.redirect('/');
       }
@@ -62,8 +62,15 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-      const product = new Product(updatedTitle,updatedPrice,updatedDesc,updatedImageUrl, new ObjectId(prodId))
-       product.save()
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    prodId
+  );
+  product
+    .save()
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -85,8 +92,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-    Product.deleteById(prodId)
-    .then(result => {
+  Product.deleteById(prodId)
+    .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
     })
--- models/product.js
+++ models/product.js
@@ -1,103 +1,78 @@
-const getDb = require('../util/database').getDb
-const mongodb = require("mongodb");
-// const { get } = require('../routes/admin');
-class Product{
-  constructor(title,price,description, imageUrl,id){
-    this.title= title;
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
+class Product {
+  constructor(title, price, description, imageUrl, id, userId) {
+    this.title = title;
     this.price = price;
-    this.description= description;
+    this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
   }
 
-  save(){
-    let db = getDb();
-    let dbOp ;
-    if(this._id){
-      dbOp= db.collection('product').updateOne({_id:this._id},{$set:this})
+  save() {
+    const db = getDb();
+    let dbOp;
+    if (this._id) {
+      // Update the product
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
     }
-    else{
-      dbOp =db.collection('product').insertOne(this)
-    }
-    
-   return dbOp
-    .then(result =>{
-      console.log(result);
-     
-    })
-    .catch(err=>{
-      console.log(err);
-      
-    })
+    return dbOp
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  static fetchAll(){
+  static fetchAll() {
     const db = getDb();
-    return db.collection('product').
-    find().toArray()
-    .then(result=>{
-      // console.log(result);
-      return result
-    })
-    .catch(err=>{
-      console.log(err);
-      
-    })
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  static findById(prodId){
+  static findById(prodId) {
     const db = getDb();
-    return db.collection('product').find({_id: new mongodb.ObjectId(prodId)}).next()
-    .then(result=>{
-      return result;
-    })
-    .catch(err=>{
-      console.log(err);
-      
-     })
+    return db
+      .collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  static deleteById(prodId){
+  static deleteById(prodId) {
     const db = getDb();
-   return db.collection('product').
-    deleteOne({_id: new mongodb.ObjectId(prodId)})
-    .then(
-      result=>{
-        console.log("deleted");
-        return result
-      }
-    )
-    .catch(err=>{
-      console.log(err);
-      
-    })
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(result => {
+        console.log('Deleted');
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
-
-
 }
-
-
-
-// const Product = sequelize.define('product', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true
-//   },
-//   title: Sequelize.STRING,
-//   price: {
-//     type: Sequelize.DOUBLE,
-//     allowNull: false
-//   },
-//   imageUrl: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   },
-//   description: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   }
-// });
 
 module.exports = Product;
