265d8de6e2118702b7359e0426be5922bbf22839
indraphand-mb <indra.phand@mindbowser.com>
2026-04-09 22:31:59 +0530

    feature : Using mongoose add all functionality

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
+const mongoose = require('mongoose')
 
 const app = express();
 
@@ -19,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('69d6514472bffc0c01cf37ab')
+  User.findById('69d7cf6a4a64b4090592951a')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user; 
       next();
     })
     .catch(err => console.log(err));
@@ -32,6 +32,20 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose.connect('mongodb+srv://indradb:123@cluster0.m863viu.mongodb.net/?appName=Cluster0').then(result=>{
+  User.findOne().then(user=>{
+    if(!user){
+      const user = new User({
+    name:"Max",
+    email :"max@test.com",
+    cart:{
+    items:[]
+    }
+  })
+  user.save()
+    }
+  })
+  
+  app.listen(3000)
+  
+}).catch(err=> console.log(err))
