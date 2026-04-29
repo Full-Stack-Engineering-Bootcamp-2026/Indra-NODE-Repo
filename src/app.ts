e853323e6d5e7b4e164f98fab8e2a71cbc3dbe22
indraphand-mb <indra.phand@mindbowser.com>
2026-04-29 06:45:30 +0530

    feat : CRUD assignment done

---
+++ src/app.ts
@@ -0,0 +1,56 @@
+import express, { Application as ExpressApp } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import {Container} from "typedi";
+import  {FeedRoutes}  from "./domains/social-media/routes/feed.routes.js"
+import 'reflect-metadata'
+
+dotenv.config();
+
+class Application {
+  public app: ExpressApp;
+  private port: number;
+  private mongoUri: string;
+
+  constructor() {
+    this.app = express();
+    this.port = parseInt(process.env.PORT || "3000", 10);
+    this.mongoUri = process.env.MONGODB_URI || "";
+
+    this.initializeMiddleware();
+    this.initializeRoutes();
+  }
+
+  private initializeMiddleware(): void {
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+  private initializeRoutes(): void {
+   const feedRoutes = Container.get(FeedRoutes);
+   this.app.use("/feed", feedRoutes.router);
+  }
+
+  private async connectDatabase(): Promise<void> {
+    try {
+      await mongoose.connect(this.mongoUri);
+      console.log("MongoDB connected");
+    } catch (error) {
+      console.error("MongoDB connection failed");
+      process.exit(1);
+    }
+  }
+
+  public async start(): Promise<void> {
+    await this.connectDatabase();
+    this.app.listen(this.port, () => {
+      console.log(`Server listening on http://localhost:${this.port}`);
+    });
+  }
+}
+
+const application = new Application();
+application.start()
+
+export default application.app;
---
+++ src/domains/social-media/types/postDto.ts
@@ -0,0 +1,10 @@
+
+
+
+export interface postCreateDto{
+    
+    title:string,
+    content:string,
+    // imageUrl :string,
+    // creator:Object
+}
